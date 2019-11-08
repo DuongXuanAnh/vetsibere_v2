ff56f913dfdb60e73b7dc87d4c83fa328984d66f
zajdabracho <46813268+zajdabracho@users.noreply.github.com>
2019-11-08 08:00:44 +0100

    Text

--- VetsiBere/Form1.cs
+++ VetsiBere/Form1.cs
@@ -189,7 +189,7 @@ namespace VetsiBere
         //---------------------------------------------------------------
         void zahajitDalsiTurnProVyhranyHrace(int pocetVyhranyHracu)
         {
-            MessageBox.Show("Aspon 2 hrace maji stejne karty");
+            MessageBox.Show("2 hráči mají stejnou kartu.");
             kolaNavic = true;
             smazatOtevrenyKarty();
             otevreneKarty = new List<Karta>();
@@ -203,7 +203,7 @@ namespace VetsiBere
 
         void sebratVsechnyKarty(Hrac vitez)
         {
-            MessageBox.Show("Bere hrac " + vitez.name);
+            MessageBox.Show("Bere hráč " + vitez.name);
 
   
 
